66b304823931786a627cbae555ed6c26b7b5f0d0
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-01-20 19:40:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -101,7 +101,7 @@ Zipping kernel..."
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
 else
-git clone -q https://github.com/neophyteprjkt/AnyKernel3 || {
+git clone -q https://github.com/malkist01/AnyKernel2 || {
 tg_msg "❌ Failed cloning AnyKernel3"
 }
 fi
